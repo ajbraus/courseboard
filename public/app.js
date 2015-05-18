a8e759b357ab26d3cd2a8af8f4a27d02596940bd
Adam Braus <ajbraus@gmail.com>
2015-05-18 13:10:10 -0700

    ngTouch

--- public/app.js
+++ public/app.js
@@ -7,14 +7,15 @@
 angular.module('myApp', ['ngResource', 
                          'ngRoute', 
                          'ngSanitize', 
+                         'ngTouch', 
                          'btford.socket-io',
                          'ngCookies',
                          'myApp.services', 
                          'myApp.directives', 
                          'myApp.interceptors'])
 
-  // .constant('HOST', 'http://localhost:1337') //DEV
-  .constant('HOST', 'http://questionqueue.herokuapp.com') //PRODUCTION
+  .constant('HOST', 'http://localhost:1337') //DEV
+  // .constant('HOST', 'http://questionqueue.herokuapp.com') //PRODUCTION
 
   .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider
