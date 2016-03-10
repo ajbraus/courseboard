0d085c79883f4efe6aac8ca10ba0cf74dadfc77b
leslie kimm <leslie.kimm@gmail.com>
2016-03-10 10:50:19 -0800

    Create courese model

---
+++ models/course.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose'),
+    bcrypt = require('bcryptjs'),
+    Schema = mongoose.Schema;
+
+// GETTER
+function toLower (v) {
+  return v.toLowerCase();
+}
+
+function toTitle(v) {
+  return v.charAt(0).toUpperCase() + v.slice(1);
+}
+
+var CourseSchema = new Schema({
+    createdAt          : Date
+  , updatedAt          : Date
+  , title              : String
+  , instructor         : String
+  , description        : String
+  // , email              : { type: String, required: true, select:false, unique: true, trim: true, set: toLower }
+  // , password           : { type: String, select: false, trim: true }
+  // , first              : { type: String, trim: true, set: toTitle }
+  // , last               : { type: String, trim: true, set: toTitle }
+  // , username           : { type: String, trim: true, set: toLower }
+  // , admin              : { type: Boolean, default: false }
+  // , confirmedAt        : { type: Date, default: undefined }
+  // , resetPasswordToken : String
+  // , resetPasswordExp   : Date
+})
+
+var Course = mongoose.model('Course', CourseSchema);
+
+module.exports = Course;
