65ea72b00980cb613bc8835ff7077af15f98fa13
leslie kimm <leslie.kimm@gmail.com>
2016-03-10 10:51:19 -0800

    create coures resource

---
+++ resources/courses.js
@@ -0,0 +1,14 @@
+
+var Course = require('../models/course.js')
+  , Question = require('../models/question.js')
+  , Answer = require('../models/answer.js')
+  , Comment = require('../models/comment.js')
+  , Tag = require('../models/tag.js')
+  , auth = require('./auth.js')
+  , request = require('request')
+  , config = require('../config.js')
+  , _ = require('lodash')
+
+
+module.exports = function(app) {
+}
